290925201529b3ce0fba3d6d25bb4c6f0351d6b0
thomas <thomas54594@gmail.com>
2026-03-03 10:10:20 +0530

    dd

--- Job-portal-main/src/App.jsx
+++ Job-portal-main/src/App.jsx
@@ -48,7 +48,6 @@ import { FileUploadHelp } from './Components-Jobseeker/FileUploadHelp'
 import { Messenger } from './Components-Jobseeker/Messenger'
 import { Chatbox } from './Components-Jobseeker/Chatbox'
 import FindTalent from './Components-Jobseeker/FindTalent'
-import Dummy from './Components-Jobseeker/dummy'
 
 
 
@@ -245,10 +244,6 @@ const router = createBrowserRouter([
 {
 path: '/Job-portal-Live/jobseeker/FindTalent',
 element: <FindTalent/>
-},
-{
-  path: '/Job-portal-Live/jobseeker/dummy',
-  element: <Dummy />  
 }
 ])
 
